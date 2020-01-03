7a970c2c485cf417f4adf11594604b96f58fdddd
Jeris Manning <the.goose.moose@gmail.com>
2020-01-02 17:43:52 -0700

    login and registration working

--- user/user-router.js
+++ user/user-router.js
@@ -1,3 +1,14 @@
+
 const router = require('express').Router();
+const DB = require('../knex-queries/model.js');
+
+router.get('/', async (req, res) => {
+  const allUsers = await DB.find(users);
+  try {
+    res.status(200).json(allUsers);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
